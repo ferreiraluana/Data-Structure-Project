3b26b03c07c29fd5a77a639dc5a87bcc6a83de68
John Davi Dutra Canuto Pires <41549047+JohnQ00@users.noreply.github.com>
2019-03-02 20:05:05 -0300

    Update Genesis.c

--- Genesis.c
+++ Genesis.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdbool.h>
 #include <limits.h>
+#include <string.h>
+
+typedef struct red_black_tree rbt;
+
 void main()
 {
+	char op,op1;
+	char op2,op3,op4;
 
+	scanf("%c", &op1); op = getchar();
+	
+	switch(op1)
+	{
+		case 'i': scanf("%d", &op3);//inserir
+		case 'p': //printar
+	}
+
+	return 0;
 }
+
+struct red_black_tree
+{
+	(*void) item; //não tenho certeza se está correto;
+	int color;//também não tenho certeza se aqui precisa ser um inteiro.
+	red_black_tree *left;
+	red_black_tree *right;
+};
