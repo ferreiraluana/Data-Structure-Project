ec432d282cbe7a9e5f0d7e37359b169d7430950b
John Davi Dutra Canuto Pires <41549047+JohnQ00@users.noreply.github.com>
2019-03-21 21:37:51 -0300

    Update deuteronomio.c
    Olhem se tá certo.

--- Priority Queue Vs Heap/deuteronomio.c
+++ Priority Queue Vs Heap/deuteronomio.c
@@ -3,78 +3,145 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <limits.h>
-#include <time.h>
-
-#define MAX 100000
+//botei a binary search do jeito que vi no slide e que lembrava e saiu isso
 typedef struct heap qheap;
 typedef struct item_of_queue item;
 
-qheap *create_qheap(int capacity);
+qheap *create_qheap(long long int capacity);
+
+void enqueue(qheap * hp, int value, int priority, int size_);
+
 int parent_index(qheap *hp, int value);
 int left_index(qheap *hp, int value);
 int right_index(qheap *hp, int value);
-int dequeue(qheap *hp);
-bool is_qheap_empty(qheap *hp);
 void swap(item *a, item *b);
-void enqueue(qheap *hp, int value, int priority);
+bool is_qheap_empty(qheap *hp);
+
 void max_qheapify(qheap *hp, int i);
-void heapsearch(qheap *hp, int start, int end, int chosen, int *comparisons);
+int heapsearch(qheap *hp,long long int chosen,long long int size, long long int comparison);
+
+int dequeue(qheap *hp);
+//void destroy(heap *hp);
+
+int main()
+{
+  long long int size_, comparison = 0; 
+  printf("Pick a size for your heap: \n");
+  scanf("%lld", &size_);
+  qheap *new_qheap = create_qheap(size_);
+
+  printf("Choose the number you want to search: \n");
+  int chosen,i; scanf("%d", &chosen);
+  for (i = 1; i <= size_; i++)
+  {
+    enqueue(new_qheap,i,i,size_);
+  }
+  printf("Heap: \n");
+  for (i = 1; i <= size_; i++)
+  {
+    printf("|%d|", dequeue(new_qheap));
+  }printf("\n");
+  printf("Iterations: \n");
+  printf("%lld\n", heapsearch(new_qheap,chosen,size_,comparison));
+
+  return 0;
+}
 
 struct heap
 {
   item *data;
   int capacity;
   int size;
-  //int priority;
+  int comparations;
 };
 
 struct item_of_queue
 {
-  int value;
-  int priority;
+  long long int value;
+  long long int priority;
 };
 
-qheap *create_qheap(int capacity)
+qheap *create_qheap(long long int capacity)
 {
-  qheap *current = (qheap*)malloc(1*sizeof(qheap));
-  current->capacity = capacity;
-  current->size = 0;
-  current->data = (item*)malloc((capacity + 1) * sizeof(item));
-  return current;
+  qheap *curr = (qheap*)malloc(1*sizeof(qheap));
+  curr->capacity = capacity;
+  curr->size = 0;
+  curr->comparations = 0;
+  curr->data = (item*)malloc((capacity + 1) * sizeof(item));
+  return curr;
 }
 
-bool is_qheap_empty(qheap *hp){return (!hp->size);}
+void enqueue(qheap *hp, int value, int priority, int size_)
+{
+    hp->data[++hp->size].priority = priority;
+    hp->data[hp->size].value= value;
+    int key_index = hp->size;
+    int parentindex = parent_index(hp,hp->size);
+    hp->comparations++;
+
+    if (hp->size > size_)
+    {
+      printf("Heap Overflow\n");
+    }
+    else
+    {
+    while (parentindex >= 1 && hp->data[key_index].priority > hp->data[parentindex].priority)
+    {
+      swap(&hp->data[key_index],&hp->data[parentindex]);
+      key_index = parentindex;
+      parentindex = parent_index(hp,key_index);
+    }
+    }
+}
+
+int heapsearch(qheap *hp, long long int chosen, long long int size, long long int comparisons)
+{
+  long long int begin = 0, end = size - 1;
+  int middle;
+
+  while(begin <= end)
+  {
+    middle = (begin + end)/2;
+    if (hp->data[middle].value < chosen)
+    {
+      comparisons++;
+      begin = middle + 1;
+    }
+    else if (hp->data[middle].value > chosen)
+    {
+      comparisons++;
+      end = middle - 1;
+    }
+    else
+    {
+      comparisons++;
+      return comparisons;
+    }
+  }
+  return 0;
+}
+
 int parent_index(qheap *hp, int i){return (i>>1);}
 int left_index(qheap *hp, int i){return (i<<1);}
 int right_index(qheap *hp, int i){return ((i<<1) + 1);}
-
-void enqueue(qheap *hp, int value, int priority)
-{ 
- hp->data[++hp->size].priority = priority;
- hp->data[hp->size].value = value;
- int key_index = hp->size;
- int parentindex = parent_index(hp,hp->size);
-
- if (is_qheap_empty(hp))
- {
-  printf("Heap Overflow\n"); // ???
-}
-else
-{
-  while (parentindex >= 1 && hp->data[key_index].priority > hp->data[parentindex].priority)
-  {
-    swap(&hp->data[key_index],&hp->data[parentindex]);
-    key_index = parentindex;
-    parentindex = parent_index(hp,key_index);
-  }
-}   
-}
 
 void swap(item *a, item *b)
 {
   item aux = *a;
   *a = *b;
   *b = aux;
+}
+
+bool is_qheap_empty(qheap *hp){ return (!hp->size);}
+
+int dequeue(qheap *hp)
+{
+  if (hp->size == 0) {printf("Heap Underflow\n"); return 0;}
+  int num = hp->data[1].value;
+  hp->data[1] = hp->data[hp->size];
+  hp->size--;
+  max_qheapify(hp,1);
+  return num;
 }
 
 void max_qheapify(qheap *hp, int i)
@@ -99,76 +166,4 @@ void max_qheapify(qheap *hp, int i)
     swap(&hp->data[i], &hp->data[largest]);
     max_qheapify(hp,largest);
   }
-}
-
-int dequeue(qheap *hp)
-{
-  if (hp->size == 0) {printf("Heap Underflow\n"); return 0;}
-  int num = hp->data[1].value;
-  hp->data[1] = hp->data[hp->size];
-  hp->size--;
-  max_qheapify(hp,1);
-  return num;
-}
-
-void heapsearch(qheap *hp, int start, int end, int chosen, int *comparisons)
-{
-  *comparisons +=1;
-  if(end >= 1)
-  {
-    int middle = (start + end)/2;
-    if(hp->data[middle].priority == chosen) 
-    {
-      return;
-    }
-    else if(hp->data[middle].priority > chosen) 
-    {
-      heapsearch(hp, start, middle-1, chosen, comparisons);
-    }
-    else if(hp->data[middle].priority < chosen) 
-    {
-      heapsearch(hp, middle + 1, end, chosen, comparisons);
-    }
-  }
-  else
-  {
-    printf("NOT FOUND\n");
-    return;
-  }
-} 
-
-void main()
-{
-  int size; printf("TYPE THE HEAP MAXIMUM SIZE:\n");
-  scanf("%d", &size); printf("\n");
-  qheap *heap = create_qheap(size); // size + 1
-  int i, j, k, chosen, comparisons = 1, size1;
-  int item, preference;
-  srand(time(NULL));
-  size1 = size;
-  while(size--)
-  {
-    item = rand() % MAX;
-    preference = item;
-    //preference = rand() % MAX;
-    enqueue(heap, item, preference); 
-  }
-  printf("HOW MANY NUMBERS DO YOU WANNA FIND?\n");
-  scanf("%d", &i); printf("\n");
-  printf("ENTER THE NUMBERS TO BE FOUND:\n");
-
-  for(k = 1; k <= i; k++)
-  {
-    scanf("%d", &chosen);
-    heapsearch(heap, 1, size, chosen, &comparisons);
-    printf("%d,%d\n", chosen, comparisons);
-    //printf("|%d|->%d\n", heap->data[k].value, heap->data[k].priority);
-    comparisons = 0;
-  }printf("\n");
-  
-  for (j = 1; j < size1; j++)
-  {
-    printf("|%d|", dequeue(heap));
-  } printf("\n");
-  free(heap);
 }
