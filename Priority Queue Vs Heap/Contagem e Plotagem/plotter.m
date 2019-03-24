7725807491645206333575fb5909cb021812f91e
Luana Ferreira <44387993+ferreiraluana@users.noreply.github.com>
2019-03-24 12:22:16 -0300

    Update plotter.m

--- Priority Queue Vs Heap/Contagem e Plotagem/plotter.m
+++ Priority Queue Vs Heap/Contagem e Plotagem/plotter.m
@@ -1,11 +1,14 @@
 data1 = csvread('priority_queue.csv');
-x1 = data(:,1);
-y1 = data(:,2);
+x1 = data1(:,1);
+y1 = data1(:,2);
 figure(1);
+subplot(1,2,1);
 plot(x1, y1, 'b');
+title('Regular Priority Queue');
 
 data2 = csvread('heap_queue.csv');
-x2 = data(:,1);
-y2 = data(:,2);
-figure(2);
+x2 = data2(:,1);
+y2 = data2(:,2);
+subplot(1,2,2);
 plot(x2, y2, 'r');
+title('Priority Queue with Heap');
