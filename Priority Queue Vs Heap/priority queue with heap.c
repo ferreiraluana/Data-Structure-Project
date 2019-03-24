f584266e7d36dd9839b453498c9e0dde0b7fcf91
John Davi Dutra Canuto Pires <41549047+JohnQ00@users.noreply.github.com>
2019-03-24 01:56:29 -0300

    Update priority queue with heap.c
    Falta só plotar e fazer uma que busque na hora

--- Priority Queue Vs Heap/priority queue with heap.c
+++ Priority Queue Vs Heap/priority queue with heap.c
@@ -3,143 +3,132 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <limits.h>
-//testem esse code pra ver se ele ta contando certa as iteracoes, testei aqui e tava certo,preciso da opinião de vocês
-typedef struct heap qheap;
-typedef struct item_of_queue item;
-
-qheap *create_qheap(int capacity);
-
-void enqueue(qheap * hp, int value, int priority,int chosen);
-
-int parent_index(qheap *hp, int value);
-int left_index(qheap *hp, int value);
-int right_index(qheap *hp, int value);
-void swap(item *a, item *b);
-bool is_qheap_empty(qheap *hp);
-
-void max_qheapify(qheap *hp, int i);
-void qheapsearch(qheap *hp,int num);
-
-int dequeue(qheap *hp);
-//void destroy(heap *hp);
-
-int main()
-{
-  int size_; scanf("%d", &size_);
-  qheap *new_qheap = create_qheap(size_);
-
-  int chosen,i; scanf("%d", &chosen);
-  for (i = 1; i <= size_; i++)
-  {
-    enqueue(new_qheap,i,i,chosen);
-  }//printf("%d", new_qheap->comparations);
-  for (i = 1; i <= size_; i++)
-  {
-    printf("|%d|", dequeue(new_qheap));
-  }
-
-  //heapsearch(new_heap,chosen,1);
-
-  return 0;
-}
-
-struct heap
-{
-  item *data;
-  int capacity;
-  int size;
-  int comparations;
-  int priority;
-};
+#include <time.h>
+//botei a binary search do jeito que vi no slide e que lembrava e saiu isso
+typedef struct qheap qheap;
+typedef struct item_of_queue item_of_queue;
 
 struct item_of_queue
 {
-  int value;
-  int priority;
+  long long int value;
+  long long int comp;
 };
 
-qheap *create_qheap(int capacity)
+struct qheap
+{
+  item_of_queue *data;
+  int capacity;
+  int size;
+  int comp;
+};
+
+qheap *create_qheap(qheap *heap, long long int capacity)
 {
   qheap *curr = (qheap*)malloc(1*sizeof(qheap));
   curr->capacity = capacity;
   curr->size = 0;
-  curr->comparations = 0;
-  curr->data = (item*)malloc((capacity) * sizeof(item));
+  curr->data = (item_of_queue*)malloc((capacity + 1) * sizeof(item_of_queue));
   return curr;
-}
 
-void enqueue(qheap *hp, int value, int priority,int chosen)
-{
-  hp->comparations = 0;
-	hp->data[++hp->size].priority = priority;
-    hp->data[hp->size].value= value;
-    int key_index = hp->size;
-    int parentindex = parent_index(hp,hp->size);
-    hp->comparations++;
-
-    if (is_qheap_empty(hp))
-    {
-      printf("Heap Overflow\n");
-    }
-    else
-    {
-    while (parentindex >= 1 && hp->data[key_index].priority > hp->data[parentindex].priority)
-    {
-      hp->comparations++;
-      swap(&hp->data[key_index],&hp->data[parentindex]);
-      key_index = parentindex;
-      parentindex = parent_index(hp,key_index);
-    }
-    }
-    if (chosen == value)
-    {
-      printf("Numero de iteracoes: %d\n", hp->comparations);
-    }
 }
 
 int parent_index(qheap *hp, int i){return (i>>1);}
 int left_index(qheap *hp, int i){return (i<<1);}
 int right_index(qheap *hp, int i){return ((i<<1) + 1);}
 
-void swap(item *a, item *b)
+void swap(item_of_queue *a, item_of_queue *b)
 {
-  item aux = *a;
+  item_of_queue aux = *a;
   *a = *b;
   *b = aux;
 }
 
-bool is_qheap_empty(qheap *hp){ return (!hp->size);}
-
-int dequeue(qheap *hp)
+int enqueue(qheap *hp, int value, int size_, int find)
 {
-  if (hp->size == 0) {printf("Heap Underflow\n"); return 0;}
-  int num = hp->data[1].value;
-  hp->data[1] = hp->data[hp->size];
-  hp->size--;
-  max_qheapify(hp,1);
-  return num;
+    hp->comp = 0;
+    hp->size++;
+    hp->data[hp->size].value= value;
+    int key_index = hp->size;
+    int parentindex = parent_index(hp,hp->size);
+    hp->comp +=1;
+    if (hp->size > size_)
+    {
+      printf("Heap Overflow\n");
+    }
+    else
+    {
+      while (parentindex >= 1 && hp->data[key_index].value > hp->data[parentindex].value)
+      {
+        hp->comp +=1;
+        swap(&hp->data[key_index],&hp->data[parentindex]); ////////////////tinha &
+        key_index = parentindex;
+        parentindex = parent_index(hp,key_index);
+      }
+    }
+
+    //if(find == value)
+      //printf("Number of iterations: %d\n", hp->comp);
+
+    return (hp->comp);
 }
 
-void max_qheapify(qheap *hp, int i)
+bool is_qheap_empty(qheap *hp){ return (!hp->size);}
+
+void max_qheapify(qheap *hp, int i, int *comp)
 {
+  (*comp)+=1;
   int largest;
   int leftindex = left_index(hp,i);
   int rightindex = right_index(hp,i);
 
-  if (leftindex <= hp->size && hp->data[leftindex].priority > hp->data[i].priority)
+  if (leftindex <= hp->size && hp->data[leftindex].value > hp->data[i].value)
   {
     largest = leftindex;
   }
   else{largest = i;}
 
-  if (rightindex <= hp->size && hp->data[rightindex].priority > hp->data[largest].priority)
+  if (rightindex <= hp->size && hp->data[rightindex].value > hp->data[largest].value)
   {
     largest = rightindex;
   }
 
-  if (hp->data[i].priority != hp->data[largest].priority)
+  if (hp->data[i].value != hp->data[largest].value)
   {
     swap(&hp->data[i], &hp->data[largest]);
-    max_qheapify(hp,largest);
+    max_qheapify(hp,largest,comp); ///ver esse &(*comp)
   }
+}
+
+FILE *plot1(FILE *f, int i, int iterat)
+{
+  f = fopen("plotFILE.csv", "a+"); /// era a+
+  //fprintf(f, "%d , %d\n", i,iterat);
+  fprintf(f, "%d;%d\n", i,iterat);
+
+  return f;
+}
+
+int main()
+{
+  int size_,num,quantity;
+  FILE *plot;
+
+  printf("Pick a size for your heap: \n");
+  scanf("%d", &size_);
+  qheap *new_qheap = create_qheap(new_qheap, size_);
+
+  int i, chosen;
+  //printf("WHAT NUMBER ARE YOU LOOKING FOR?\n");
+  //scanf("%d", &chosen);
+
+  srand(time(NULL));
+  for (i = 1; i <= size_; i++)
+  {
+    num = rand();
+    quantity = enqueue(new_qheap, num, size_, chosen);
+    plot = plot1(plot,i, quantity);
+  }
+  fclose(plot);
+
+  return 0;
 }
