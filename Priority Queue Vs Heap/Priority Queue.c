3a474430428dea389053770f3c3d254d610bb784
John Davi Dutra Canuto Pires <41549047+JohnQ00@users.noreply.github.com>
2019-03-24 00:46:15 -0300

    Update Priority Queue.c
    Agora tem o csv, mas não sei ajeitar direito

--- Priority Queue Vs Heap/Priority Queue.c
+++ Priority Queue Vs Heap/Priority Queue.c
@@ -3,48 +3,60 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <time.h>
 
 typedef struct priority_queue p_q;
 typedef struct node list;
 
-p_q *create_priority_queue();
+p_q *create_priority_queue(int runs);
 list *create_node(int value, int priority);
 
 bool is_empty(p_q *pq);
 
-list *front(p_q *pq);
-int max(p_q *pq);
 void print_priority_queue(p_q *pq);
-void enqueue(p_q *pq, int value, int priority);
+p_q *enqueue(p_q *pq, int value, int priority);
 
 void dequeue(p_q *pq);
 void destroy(p_q *pq);
 
 int main(void)
 {
-  p_q *pq = create_priority_queue();
+	int runs;
+  printf("Pick your queue size: ");
+  scanf("%d", &runs);
+  p_q *pq = create_priority_queue(runs);
   printf("%s\n", is_empty(pq) == true ? "true" : false);
 
   char op1,op;
-  int op2,op3,op4,priority,runs,i = 0;
+  int op2,op3,op4,priority,i = 0;
+  //srand(time(NULL));
 
-  printf("Insira o numero de itens que voce deseja adicionar: ");
-  scanf("%d", &runs);
+  int j,chosen,cpriority,counter = 0;
+    int num;
+    printf("Numero da busca: \n");
+    scanf("%d", &chosen);
+    for (i = 1; i <= runs; i++)
+    {
+      //num = rand();
+      enqueue(pq,i,i);
+    }
 
-  while(i < runs)
+  /*while(i < runs)
   {
   	printf("Insira o valor: ");
   	scanf("%d", &op2);
   	printf("Insira a prioridade desse valor: ");
   	scanf("%d", &priority);
 
-  	enqueue(pq,op2,priority);
+
   	i++;
-  }
-  print_priority_queue(
-  pq);
-  /*
-  do
+  }*/
+
+  	print_priority_queue(pq);
+
+  	//enqueue(pq,INT_MAX,INT_MAX,chosen);
+
+  /*do
   {
   	scanf("%c", &op1); op = getchar();
   switch (op1)
@@ -66,6 +78,9 @@ int main(void)
 struct priority_queue
 {
   list *head;
+  int max_size;
+  int comparisons;
+  int size;
 };
 
 struct node
@@ -75,19 +90,13 @@ struct node
   list *next;
 };
 
-p_q *create_priority_queue()
+p_q *create_priority_queue(int runs)
 {
   p_q *current = (p_q*)malloc(1 * sizeof(p_q));
   current->head = NULL;
-  return (current);
-}
-
-list *create_node(int value, int priority)
-{
-  list *current = (list*)malloc(1 * sizeof(list));
-  current->value = value;
-  current->priority = priority;
-  current->next = NULL;
+  current->comparisons = 0;
+  current->size = 0;
+  current->max_size = runs;
   return (current);
 }
 
@@ -97,21 +106,16 @@ list *front(p_q *pq){return (pq->head);}
 
 int max(p_q *pq){return(pq->head->value);}
 
-void print_priority_queue(p_q *pq)
-{
-  list *current = pq->head;
-  while(current->next != NULL)
-  {
-    printf("%d ", current->value);
-    current = current->next;
-  } printf("%d\n", current->value);
-}
 
-void enqueue(p_q *pq, int value, int priority)
+p_q *enqueue(p_q *pq, int value, int priority)
 {
-  list *current = create_node(value,priority);
+  list *current = (list*)malloc(sizeof(list));
+  current->value = value;
+  current->priority = priority;
   if (is_empty(pq) || current->priority > pq->head->priority)
   {
+    if (!is_empty(pq)){pq->comparisons++;}
+    //current->comparisons++;
     current->next = pq->head;
     pq->head = current;
   }
@@ -120,11 +124,33 @@ void enqueue(p_q *pq, int value, int priority)
     list *current_l  = pq->head;
     while(current_l->next != NULL && current->priority < current_l->next->priority)
     {
+    	pq->comparisons++;
+    	//(current->comparisonsarisons++);
       current_l = current_l->next;
     }
     current->next = current_l->next;
     current_l->next = current;
   }
+  pq->size++;
+  if (pq->size == pq->max_size)
+  {
+    FILE *output;
+    output = fopen("ame3.csv", "a");
+    fprintf(output, "%d, %d\n", pq->size, pq->comparisons);
+    fclose(output);
+  }
+  //current->comparisons++;
+  //return comparisons;
+}
+
+void print_priority_queue(p_q *pq)
+{
+  list *current = pq->head;
+  while (current->next != NULL)
+  {
+    printf("|%d|", current->value);
+    current = current->next;
+  } printf("|%d|\n", current->value);
 }
 
 void dequeue(p_q *pq)
